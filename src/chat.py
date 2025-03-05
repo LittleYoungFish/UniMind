feb189d45b1594ca6a8bb71e28f2088d6ef957cb
Kcornw <141114704+kcornw@users.noreply.github.com>
2025-03-05 19:33:26 +0800

    refactor: add comment

--- src/chat.py
+++ src/chat.py
@@ -7,14 +7,26 @@ from openai.types.chat.chat_completion import ChatCompletion
 
 
 @dataclass
+class Usage:
+    """
+    Dataclass to represent the usage statistics of a chat model response.
+    """
+
+    prompt: int
+    completion: int
+
+
+@dataclass
 class Response:
-    @dataclass
-    class Usage:
-        prompt: int
-        completion: int
+    """
+    Dataclass to represent a response from the chat model.
+    """
 
     content: str
     usage: Usage
+
+    def __str__(self):
+        return f"<Response> ({self.content[:50]})"
 
 
 class Role:
@@ -31,7 +43,20 @@ class Role:
         "max_completion_tokens",
     ]
 
-    def __init__(self, prompt):
+    def __init__(self, prompt: str):
+        """
+        Initialize the Role class with a prompt.
+        Loads environment variables and sets up the OpenAI client.
+        Validates the API key and model.
+        Defaults to GPT-4o-mini if no model is specified.
+
+        Parameters:
+            prompt (str): The initial prompt for the chat session.
+
+        Raises:
+            ValueError: If the API key is not provided or the model is not specified.
+        """
+
         is_env_loaded = load_dotenv()
 
         api_key = os.getenv("API_KEY", "")
@@ -49,6 +74,20 @@ class Role:
         self.model = model
 
     def chat(self, query: str, **generation_params) -> Response:
+        """
+        Handles the chat interaction with the specified model.
+
+        Parameters:
+            query (str): The user's query or message.
+            **generation_params: Additional parameters for the generation process.
+
+        Returns:
+           Response: The response from the model.
+
+        Raises:
+            ValueError: If an unsupported generation parameter is provided.
+        """
+
         for param in generation_params.keys():
             if param not in self.ALLOWED_GEN_PARAM:
                 raise ValueError(f"Generation param {param} not implemented yet")
