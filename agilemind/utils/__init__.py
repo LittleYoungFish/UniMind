ae80b0f0c3ad6e6aea17f030b9e05bb0f80f712f
WnRock <141114704+kcornw@users.noreply.github.com>
2025-04-03 09:59:29 +0800

    feat: add framework extraction utility for Python files

--- agilemind/utils/__init__.py
+++ agilemind/utils/__init__.py
@@ -6,6 +6,7 @@ from .retry import retry
 from .cost import format_cost
 from .window import LogWindow
 from .model_pricing import calculate_cost
+from .code_framework_extractor import extract_framework
 from .json_cleaner import extract_json, clean_json_string
 from .config_loader import load_config, extract_agent_llm_config
 from .json_to_markdown import convert as convert_json_to_markdown, create_file_tree
@@ -16,6 +17,7 @@ __all__ = [
     "load_config",
     "LogWindow",
     "calculate_cost",
+    "extract_framework",
     "extract_json",
     "clean_json_string",
     "extract_agent_llm_config",
